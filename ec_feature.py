aaded2ce62a802a9ca265c76d6c55eb08f2cf764
Kazuma1921 <13727@enautica.pt>
2025-03-25 16:53:50 +0000

    ec_feature

--- ec_feature.py
+++ ec_feature.py
@@ -1,9 +1,11 @@
 import pytesseract
+import cv2
 
 def extract_text_from_image(image_path):
     try:
-        text = pytesseract.image_to_string(image_path, lang="por")
-        return text
+        custom_config = '--oem 1 --psm 4 -c tessedit_char_whitelist="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.€@()/"'
+        text = pytesseract.image_to_string(image_path, lang="por", config=custom_config)
+        return text.strip()
     except Exception as e:
         print(f"Erro ao processar a imagem {image_path}: {e}")
         return ""
